315ccf909efe662a6551617dc0f8280609c8202e
Oğuzcan YAVUZ <oguzcanyavuz321@gmail.com>
2023-01-25 11:19:55 +0300

    fix: telegram bot import

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
-// import * as TelegramBot from 'node-telegram-bot-api';
-const TelegramBot = require('node-telegram-bot-api');
-import { format } from 'date-fns'
+import TelegramBot, { Message } from 'node-telegram-bot-api';
 
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
-    const body = JSON.parse(event.body ?? '');
-    // const message = body.message as TelegramBot.Message;
-    const message = body.message;
-    console.log({ message, chat: message.chat })
+    try {
+        const body = JSON.parse(event.body ?? '');
+        const message = body.message as Message;
+        console.log({ message, chat: message.chat })
 
-    console.log('datefn');
-    const d = format(new Date(), "'Today is a' eeee")
-    console.log({ d })
+        const token = '';
+        console.log('here')
+        const bot = new TelegramBot(token);
+        console.log('here2')
 
+        await bot.sendMessage(message.chat.id, 'pong')
 
-    const token = '';
-    console.log('here')
-    const bot = new TelegramBot(token, { webHook: true });
-    console.log('here2')
-
-    await bot.sendMessage(message.chat.id, 'pong')
-
-    console.log('here3')
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: event.body })
+        console.log('here3')
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'success' })
+        }
+    } catch (err) {
+        console.error(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'failure' })
+        }
     }
 }
