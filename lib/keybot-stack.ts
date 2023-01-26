42087ac63224ec3af88a8908791a12455d4fa5d7
Oğuzcan YAVUZ <oguzcanyavuz321@gmail.com>
2023-01-26 20:44:10 +0300

    feat: spotify oauth integration

--- lib/keybot-stack.ts
+++ lib/keybot-stack.ts
@@ -2,10 +2,12 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as lambda from "aws-cdk-lib/aws-lambda";
-import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
-import * as apigateway from "aws-cdk-lib/aws-apigateway";
-import * as logs from "aws-cdk-lib/aws-logs"
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as logs from 'aws-cdk-lib/aws-logs'
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager'
+import { IRole } from 'aws-cdk-lib/aws-iam';
 import * as path from 'path';
 
 
@@ -13,22 +15,26 @@ export class KeybotStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const handler = new NodejsFunction(this, "KeybotHandler", {
+    const handler = new NodejsFunction(this, 'KeybotHandler', {
       runtime: lambda.Runtime.NODEJS_18_X,
-      entry: path.join(__dirname, "../src/index.ts"),
+      entry: path.join(__dirname, '../src/index.ts'),
       handler: 'handler',
       bundling: {
-        externalModules: ['aws-sdk'],
+        externalModules: ['aws-sdk', '@aws-sdk'],
         minify: true,
         sourceMap: true,
         target: 'es2020'
       },
       environment: {
         telegramBotToken: process.env.TELEGRAM_BOT_TOKEN ?? '',
+        spotifyClientId: process.env.SPOTIFY_CLIENT_ID ?? '',
+        spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET ?? '',
+        spotifyRedirectUrl: process.env.SPOTIFY_REDIRECT_URL ?? '',
+        region: 'eu-central-1',
       },
     })
 
-    const logGroup = new logs.LogGroup(this, "ApiGatewayAccessLogs_Keybot");
+    const logGroup = new logs.LogGroup(this, 'ApiGatewayAccessLogs_Keybot');
     const accessLogFormat = JSON.stringify({
       requestId: '$context.requestId',
       userAgent: '$context.identity.userAgent',
@@ -39,9 +45,9 @@ export class KeybotStack extends cdk.Stack {
       status: '$context.status',
       responseLength: '$context.responseLength',
     })
-    const api = new apigateway.RestApi(this, "keybot-api", {
-      restApiName: "Keybot API",
-      description: "Keybot API",
+    const api = new apigateway.RestApi(this, 'keybot-api', {
+      restApiName: 'Keybot API',
+      description: 'Keybot API',
       cloudWatchRole: true,
       deployOptions: {
         accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
@@ -51,9 +57,14 @@ export class KeybotStack extends cdk.Stack {
 
     const integration = new apigateway.LambdaIntegration(handler);
 
-    const v1 = api.root.addResource("v1")
-    const keys = v1.addResource("keys")
+    const v1 = api.root.addResource('v1')
+    const keys = v1.addResource('keys')
 
     keys.addMethod('POST', integration)
+
+    const spotifyAccessTokenSecret = new secretsmanager.Secret(this, 'SpotifyAccessTokenSecret');
+    spotifyAccessTokenSecret.grantRead(handler.role as IRole)
+    spotifyAccessTokenSecret.grantWrite(handler.role as IRole)
+    handler.addEnvironment('spotifySecretArn', spotifyAccessTokenSecret.secretArn)
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,57 @@
-import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import { APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import { SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-secrets-manager";
 import TelegramBot, { Message } from 'node-telegram-bot-api';
+import SpotifyWebApi from 'spotify-web-api-node';
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.spotifyClientId,
+    clientSecret: process.env.spotifyClientSecret,
+    redirectUri: process.env.spotifyRedirectUrl,
+});
+
+const generateSpotifyAuthUrl = (chatId: number): string => {
+    const scopes = ['user-read-currently-playing']
+
+    // TODO: return the auth url only if the token is expired or it is non existent
+    // Create the authorization URL
+    const authorizeURL = spotifyApi.createAuthorizeURL(scopes, chatId.toString());
+
+    console.log({ authorizeURL })
+
+    return authorizeURL
+}
 
 export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
     try {
-        const body = JSON.parse(event.body ?? '');
-        const message = body.message as Message;
+        const body = JSON.parse(event.body ?? '')
+        const message = body.message as Message
         console.log({ message })
 
         const token = process.env.telegramBotToken as string;
         const bot = new TelegramBot(token);
+        const secretsClient = new SecretsManagerClient({ region: process.env.region });
 
-        await bot.sendMessage(message.chat.id, 'pong')
+        const spotifySecret = await secretsClient.send(new GetSecretValueCommand({
+            SecretId: process.env.spotifySecretArn,
+        }))
+        console.log({ spotifySecret })
+
+        // const currentTrack = await spotifyApi.getMyCurrentPlayingTrack()
+        // console.log({ currentTrack })
+        // const trackId = currentTrack.body.item?.id
+        // console.log({ trackId })
+        const chatId = message.chat.id
+        const authUrl = generateSpotifyAuthUrl(chatId)
+
+        const telegramMessage = `${authUrl} secret: ${spotifySecret.SecretString}`
+        await bot.sendMessage(chatId, telegramMessage)
 
         return {
             statusCode: 200,
             body: JSON.stringify({ message: 'success' })
         }
     } catch (err) {
-        console.error(err);
+        console.error(err)
         return {
             statusCode: 500,
             body: JSON.stringify({ message: 'failure' })
